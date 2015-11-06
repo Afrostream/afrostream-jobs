5400f994cfefb5b7f5d4da7fcf2e7353ddc28c85
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-06 14:17:50 +0100

    job: pack captions: simulating job behavior

--- jobs/pack-captions.js
+++ jobs/pack-captions.js
@@ -2,18 +2,8 @@
 
 module.exports = function (job, ctx) {
   console.log('processing pack-captions');
-  job.subscribe(function () {
-    job.on('job process: remove', function () {
-      console.log('removed');
-    });
-    job.on('job process: failed', function () {
-      console.log('failed');
-    });
-    job.on('job process: failed attempt', function () {
-      console.log('failed attempt');
-    });
-    job.on('job process: complete', function () {
-      console.log('complete');
-    });
-  });
+  setTimeout(function () {
+    // simulating call to afrostream-packaging & answer after 5s
+    require('../app/delayed.js').success(job.id, function () {});
+  }, 5000);
 };
