614c072fec51bb4a0cf836d66eec55a38cbbb1d8
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-02-03 14:25:49 +0100

    bugfix: watchdog for stuck jobs

--- kue-queue.js
+++ kue-queue.js
@@ -5,6 +5,10 @@ var config = require('./config');
 var kue = require('kue')
   , queue = kue.createQueue(config.redis);
 
+// redis connections can be unstable
+// https://github.com/Automattic/kue#unstable-redis-connections
+queue.watchStuckJobs(1000);
+
 // error handling: global try/catch with restart...
 queue.on('error', console.error.bind(console));
 
